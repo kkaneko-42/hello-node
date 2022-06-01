e9341d67c3611c06a568ecf59a6b74804af369e8
kkaneko <kkaneko@student.42tokyo.jp>
2022-06-01 12:14:17 +0900

    ex00 functionable. ex01 create

---
+++ ex01/vars.js
@@ -0,0 +1,13 @@
+const ft_str = "42";
+const ft_num = 42;
+const ft_obj1 = {ft_str};
+const ft_obj = {
+	"object": "Object"
+};
+const ft_bool = true;
+const ft_und = undefined;
+console.log(ft_str, " is a ", typeof(ft_str));
+console.log(ft_num, " is a ", typeof(ft_num));
+console.log(ft_obj1, " is a ", typeof(ft_obj1));
+console.log(ft_obj, " is a ", typeof(ft_obj));
+console.log(ft_und, " is a ", typeof(ft_und));
